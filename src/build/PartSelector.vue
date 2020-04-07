3fc54f172122c964c8a5bf8df35f4138b072a410
Tuan Luu <tuan.luu@asnet.com.vn>
2020-04-07 23:56:28 +0700

    Chapter 4 - Enabling inter-componend communication

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="part">
+  <div class="part" :class="position">
     <img :src="selectedPart.src" title="arm" alt="" />
     <button @click="selectedPreviousPart()" class="prev-selector"></button>
     <button @click="selectedNextPart()" class="next-selector"></button>
@@ -19,7 +19,19 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  props: ['parts'],
+  props: {
+    parts: {
+      type: Array,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+      validator(value) {
+        return ['left', 'right', 'top', 'bottom', 'center'].includes(value);
+      },
+    },
+  },
   data() {
     return { selectedPartIndex: 0 };
   },
@@ -28,18 +40,26 @@ export default {
       return this.parts[this.selectedPartIndex];
     },
   },
+  created() {
+    this.emitSelectedPart();
+  },
   methods: {
+    emitSelectedPart() {
+      this.$emit('partSelected', this.selectedPart);
+    },
     selectedNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.emitSelectedPart();
     },
     selectedPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.emitSelectedPart();
     },
   },
 };
